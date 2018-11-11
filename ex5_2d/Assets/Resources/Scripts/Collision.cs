f31199c3a98b767b2a9fdc183c2685b9eb383f7c
vcabales <vcabales96@gmail.com>
2018-11-11 00:24:08 -0600

    collisions added

---
+++ ex5_2d/Assets/Resources/Scripts/Collision.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("detecting collision");
+        Debug.Log(other);
+        //Destroy(gameObject);
+        if (Time.time > 2f) {
+            if (other.GetComponent<GuMovement>())
+            {
+                Destroy(other.gameObject);
+            }
+            if (other.GetComponent<GuMovement>() || other.GetComponent<PlayerController>())
+                Destroy(gameObject);
+        }
+    }
+    private void Update()
+    {
+        if (gameObject != null) {
+            var rb = GetComponent<Rigidbody2D>();
+            Debug.Log(rb.transform.position);
+            if (rb.transform.position.y < -10f)
+            { // Figure out how to adjust this
+                Debug.Log("here");
+                if (gameObject)
+                    Destroy(gameObject);
+            }
+        }
+    }
+}
--- ex5_2d/Assets/Resources/Scripts/FoodManager.cs
+++ ex5_2d/Assets/Resources/Scripts/FoodManager.cs
@@ -10,28 +10,31 @@ public class FoodManager : MonoBehaviour {
     public GameObject Food;
     public Transform foodTransform;
     public static System.Random rand = new System.Random();
-    public float timer = 2f;
+    public float timer = 1f;
 
     // Update is called once per frame
     void Update () {
         timer -= Time.deltaTime;
         if (timer <= 0f) {
             SpawnFood();
-            timer = 2f;
+            timer = 1f;
         }
             
 	}
     void SpawnFood() {
-        Debug.Log("Spawning food");
         var cam = Camera.main;
         //cam.ViewportToWorldPoint(new Vector3(1, 1, cam.nearClipPlane)); // top right
         var tryThis = cam.ViewportToWorldPoint(new Vector3(0, 1, cam.nearClipPlane)); // top left
-        // Otherwise, put in 4.84 for y for now
-        Vector2 spawnPosition = new Vector2(Random.Range(minX, maxX), 4.84f);
-        //foodTransform.position = spawnPosition;
+        Vector2 spawnPosition = new Vector2(Random.Range(minX, maxX), tryThis.y); 
+        if (Food == null) {
+            Debug.Log(Resources.Load("./Prefabs/Food"));
+            Food = Resources.Load("Prefabs/Food") as GameObject;
+        }
+            
+        Debug.Log(Food);
         var f = Instantiate(Food);
         f.transform.position = spawnPosition;
-        int r = rand.Next(0,spriteList.Length-1);
+        int r = rand.Next(0,spriteList.Length);
         f.GetComponent<SpriteRenderer>().sprite = spriteList[r];
 
     }
